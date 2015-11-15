812c0c95bd041da6b7f5e2ba9b960de5d1efb472
Atul Shree <notul.atul@outlook.com>
2015-11-15 22:41:50 +0530

    Fibonacci Heap Node

---
+++ FBHeapNode.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author atul
+ */
+public class FBHeapNode {
+
+    int graphNode1;
+    int graphNode2;
+
+    int forrestNode1;
+    int forrestNode2;
+
+    float value;
+
+    int degree;
+
+    FBHeapNode parent;
+    FBHeapNode next;
+    FBHeapNode previous;
+    FBHeapNode childHead;
+    FBHeapNode childTail;
+}
